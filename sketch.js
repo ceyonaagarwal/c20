bec801b01cf574b9a3a8ad76bcc7483047a37455
ceyonaagarwal <70641231+ceyonaagarwal@users.noreply.github.com>
2020-09-04 20:52:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,8 +4,10 @@ function setup() {
   createCanvas(1200,800);
   fixedRect=createSprite(600, 400, 50, 80);
   fixedRect.shapeColor="green";
+  fixedRect.velocityY=5;
   movingRect=createSprite(400,200,80,30);
   movingRect.shapeColor="green";
+  movingRect.velocityY=-5;
 }
 
 function draw() {
@@ -24,6 +26,19 @@ function draw() {
   {
     movingRect.shapeColor="green";
     fixedRect.shapeColor="green";
+  }
+  //bounceOff algorithm
+  if (movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2
+      && fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2)
+  {
+    movingRect.velocityX=movingRect.velocityX*(-1);
+    fixedRect.velocityX=fixedRect.velocityX*(-1);
+  }
+  if (movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2
+      && fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2)
+  {
+    movingRect.velocityY=movingRect.velocityY*(-1);
+    fixedRect.velocityY=fixedRect.velocityY*(-1);
   }
   drawSprites();
 }
